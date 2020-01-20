712699f4a61cf2ce69c018ba71c9be083a3416a5
unknown <oricarmi92@gmail.com>
2020-01-20 17:07:01 +0200

    changed main file

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def plotData():
     
     
 freqs = [0.5,1,1.5,2,2.5,3.15,4,5,6.3,8,10,12.5,16,20,25,31.5,40,50,63,80,100,125,160,200,250]
-counter = np.zeros(26,dtype = int) # first 25 are freqs, 26 is energy
+counter = np.zeros((30,26),dtype = int) # first 25 are freqs, 26 is energy
 x = np.arange(600)
-for k in range(counter.size):
+for k in range(counter.shape[1]): # iterate the frequencies
     for i in range(4):
         temp = np.random.normal(i*k/2,1,int(x.size/4))
         if i == 0:
@@ -32,12 +32,11 @@ for k in range(counter.size):
     slope, intercept, r, p, std_err = sps.linregress(x, y)
     
     Cross = predictCrossTime(4) 
-    print(Cross)
     if Cross>0 and Cross<360: # If it will cross in less than 1 hour (number of 10 seconds in an hour)
-        counter[k] +=1
+        counter[29,k] +=1
     else:
-        counter[k] = 0
-    if counter[k] >= 60: # if it will cross in less than 1 hour for 10 minutes straight
+        counter[29,k] = 0
+    if np.sum(counter[:,k]) >= 0.8*counter.shape[0]: # if in the last 5 minutes it will cross in less than 1 hour in 80% of the times
         if k<len(freqs):
             txtstr = 'trend detected in freq %d' %freqs[k]
         else:
