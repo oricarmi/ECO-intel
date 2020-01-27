d6752ae5c3132b36e7b8df46a9dcfe6e61c93a76
unknown <oricarmi92@gmail.com>
2020-01-27 18:56:46 +0200

    mail works. all running, need to change thresh so that is array of thresholds per channel per frequency

--- AMQP.py
+++ AMQP.py
@@ -12,45 +12,55 @@ import os.path
 from scipy import stats as sps
 from matplotlib import pyplot as plt
 import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+def sendEmail(bodyTxt,imgPath):
+    gmail_user = 'carmimeister@gmail.com'
+    gmail_password = '918273645'
+    to = ['Yair@eco-eng.co.il', 'Amit@eco-eng.co.il']
+    msg = MIMEMultipart()    # instance of MIMEMultipart 
+    msg['From'] = gmail_user # storing the senders email address       
+    msg['To'] = ", ".join(to) # storing the receivers email address  
+    msg['Subject'] = "Intel Anomaly Alert!" # storing the subject  
+    body = 'Trend Detected - ' + bodyTxt # string to store the body of the mail 
+    msg.attach(MIMEText(body, 'plain')) # attach the body with the msg instance 
+    filename = 'test' + '.png' 
+    attachment = open(imgPath, "rb") # open the file to be sent  
+    p = MIMEBase('application', 'octet-stream') # instance of MIMEBase and named as p 
+    p.set_payload((attachment).read())  # To change the payload into encoded form 
+    encoders.encode_base64(p)  # encode into base64 
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+    msg.attach(p) # attach the instance 'p' to instance 'msg' 
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+#        server.starttls()
+        server.login(gmail_user, gmail_password)
+        text = msg.as_string() 
+        server.sendmail(gmail_user, to, text)
+        server.close()
+        print('email sent')
+    except:
+        print('Something went wrong...')
+    return
 
 def plotDataAndRegression(y,TitlePlt,TitleWord,toSave = 1):
     plt.figure()
     plt.plot(x, y, 'o', label='original data')
     plt.plot(x, intercept + slope*x, 'r', label='fitted line')
     plt.legend()
-    plt.title(TitlePlt + " ,R-squared: %f" % r**2)
+    plt.title(TitlePlt + ", " + TitleWord + " ,R-squared: %f" % r**2)
     if toSave:
-        plt.savefig(os.path.join(r'C:\Users\user\Desktop\Oric\ECO\intel - python',TitleWord + '.png'))
+        plt.savefig(os.path.join(r"C:\Users\Ori\Desktop\Ori\ECO\intel - python\anomaly detection\test.png")) #r'C:\Users\user\Desktop\Oric\ECO\intel - python'
     plt.close()
+    sendEmail(TitleWord,r"C:\Users\Ori\Desktop\Ori\ECO\intel - python\anomaly detection\test.png")
     return
 def linearModel(x):
     return slope*x+intercept
 def predictCrossTime(threshold = 3):
     return (threshold - intercept)/slope
-def sendEmail():
-    gmail_user = 'carmimeister@gmail.com'
-    gmail_password = '918273645'
-    sent_from = gmail_user
-    to = ['Yair@eco-eng.co.il', 'Amit@eco-eng.co.il']
-    subject = 'Test from python'
-    body = 'Hey, Test'
-    
-    email_text = """\
-    From: %s
-    To: %s
-    Subject: %s
-    
-    %s
-    """ % (sent_from, ", ".join(to), subject, body)
-    try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.ehlo()
-        server.login(gmail_user, gmail_password)
-        server.sendmail(sent_from, to, email_text)
-        server.close()
-        print('email sent')
-    except:
-        print('Something went wrong...')
 
 def msg_receive(ch, method, properties, body):
     ######## Parse incoming message #######
@@ -61,9 +71,9 @@ def msg_receive(ch, method, properties, body):
     if not method.routing_key in dataTbls:
         dataTbls[method.routing_key] = np.zeros((200,26))
         counterTbls[method.routing_key] = np.zeros((200,26))
-        timestmpsTbls[method.routing_key] = []
+        timestmpsTbls[method.routing_key] = [] # timestamps list
     data = json.loads(body)
-    timestmpsTbls[method.routing_key] = data["results"][0]['timestamp'] # get timestamp
+    timestmpsTbls[method.routing_key].append(data["results"][0]['timestamp']) # get timestamp
     spectrum = data["results"][0]['values'] # get freq values
     freqs = [] # frequencies list
     frqVals = [] # frequencies values list
@@ -76,22 +86,30 @@ def msg_receive(ch, method, properties, body):
             continue
     frqVals = np.asarray(frqVals) # make list to numpy array
     frqVals = np.concatenate((frqVals,[np.sqrt(np.mean(frqVals**2))])) # concatenate rms
-    dataTbls[method.routing_key] = np.vstack((frqVals,dataTbls[method.routing_key][0:-1,:])) # concatenate new frequencies to already existing
+    dataTbls[method.routing_key] = np.vstack((dataTbls[method.routing_key][1:,:],frqVals)) # concatenate new frequencies to already existing
     ind = 0
+    flag = 0
     for k in dataTbls[method.routing_key].T: # iterate the rows of y and perform linear regression
         slope, intercept, r, p, std_err = sps.linregress(x, k)
-        Cross = predictCrossTime(np.mean(k)+3*np.std(k)) # predict time it will cross threshold if continues in this trend
-        if Cross>0 and Cross<360: # If it will cross in less than 1 hour (number of 10 seconds in an hour)
-            counterTbls[method.routing_key][:,ind] = np.concatenate(([1], counterTbls[method.routing_key][0:-1,ind])) # append 1 to counter
+        if not flag: # if hasn't crossed, continue to check against updated mean and std
+            Cross = predictCrossTime(np.mean(k)+3*np.std(k)) # predict time it will cross threshold if continues in this trend (threshold set as mean of last half hour plus 3*std of last half hour)
+        else: # if it crossed, flag is 1 and compare to last threshold (last mean+3std that it crossed)
+            Cross = predictCrossTime(thresh)
+        if Cross>0 and Cross<360 and slope>0: # If it will cross in less than 1 hour (number of 10 seconds in an hour)
+            counterTbls[method.routing_key][:,ind] = np.concatenate((counterTbls[method.routing_key][1:,ind],[1])) # append 1 to counter
+            thresh = np.mean(k)+3*np.std(k)
+            flag = 1
         else:
-            counterTbls[method.routing_key][:,ind] = np.concatenate(([0], counterTbls[method.routing_key][0:-1,ind])) # append 0 to counter
+            counterTbls[method.routing_key][:,ind] = np.concatenate((counterTbls[method.routing_key][1:,ind],[0])) # append 0 to counter
+            flag = 0
         if np.sum(counterTbls[method.routing_key][:,ind]) >= 0.8*counterTbls[method.routing_key].shape[0]: # if in the last N minutes it will cross in less than 1 hour in 80% of the times, alert
             if ind<len(freqs):
                 txtstr = 'trend detected in freq %d [Hz]' %freqs[ind]
             else:
                 txtstr = 'trend detected in RMS'
             plotDataAndRegression(k,txtstr,method.routing_key+timestmpsTbls[method.routing_key])
-#            win32api.MessageBox(0,txtstr,'alert')
+            counterTbls[method.routing_key][:,ind] = np.zeros(200) # make zeros to not alert many times
+            print(txtstr)
         ind+=1   
 
 if __name__ == "__main__":
