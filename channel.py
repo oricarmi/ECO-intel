18ffd45f1e36d03bf483064e80135c83a2fc1990
unknown <oricarmi92@gmail.com>
2020-02-13 11:34:53 +0200

    channel class

--- channel.py
+++ channel.py
@@ -5,41 +5,83 @@ Created on Wed Jan 29 10:38:28 2020
 @author: Ori
 """
 
-import pika
 import json 
 import numpy as np
-import win32api
 import os.path
 from scipy import stats as sps
 from matplotlib import pyplot as plt
-import smtplib
-from email.mime.multipart import MIMEMultipart 
-from email.mime.text import MIMEText 
-from email.mime.base import MIMEBase 
-from email import encoders 
-
+from scipy.stats import norm
+import datetime
 class channel:
     def __init__(self, numPtsBack,name):
         self.name = name
-        self.dataTbl = np.zeros((numPtsBack,26)) # dictionary, keys are sensor names and values are spectrums
-        self.counterTbl =np.zeros((numPtsBack,26))  # dictionary, keys are sensor names and values are binary counters if trend detected or not
-        self.timestmps = [] # dictionary, keys are sensor names and values are timestamps of corresponding line in dataTbls/counterTbls
-        self.thrshlds = -1*np.ones(26) #dictionary, keys are sensor names and values are thresholds
+        self.numPtsBack = numPtsBack
+        self.dataTbl = [] # list of values are spectrums
+        self.counterTbl = np.zeros((numPtsBack,26))   # binary counters if trend detected or not
+        self.timestmps = [] # timestamps of corresponding line in dataTbls/counterTbls
+        self.thrshlds = -1*np.ones(26) # thresholds of each frequency and RMS
+        self.x = np.arange(numPtsBack) # last numPtsBack points
+        self.bslnDist = {'N': 1, 'mu': np.zeros(26), 'signma': np.zeros(26) }
     
-    def parseInput(self, ch, method, properties, body):
-        data = json.loads(body)
-        self.timestmpsTbls[method.routing_key].append(data["results"][0]['timestamp']) # get timestamp
-        spectrum = data["results"][0]['values'] # get freq values
-        freqs = [] # frequencies list
-        frqVals = [] # frequencies values list
-        for f in spectrum.keys(): # iterate values and make one vector
-            try:
-                if float(f[0:-2])<=250:
-                    freqs.append(float(f[0:-2]))
-                    frqVals.append(float(spectrum[f])) 
-            except:
+    def appendDataLine(self,data,timestmp):
+        if datetime.datetime.day(self.timestmps[-1]) == day(timestmp):
+            self.dataTbl = self.dataTbl.append(data)
+            self.timestmps = self.timestmps(timestmp)
+        else:
+            self.DayEnd()
+            
+            
+    def kl_divergence(p, q):
+        return np.sum(np.where(p != 0, p * np.log(p / q), 0))    
+    
+    
+    def detectTrend(self,freqs):  
+        ind = 0 # index for frequencies and RMS ([0,25])
+        for k in self.dataTbls.T: # iterate the columns of y and perform linear regression
+            if k.size<numPtsBack:
                 continue
-        frqVals = np.asarray(frqVals) # make list to numpy array
-        frqVals = np.concatenate((frqVals,[np.sqrt(np.mean(frqVals**2))])) # concatenate rms
-        self.dataTbls[method.routing_key] = np.vstack((self.dataTbls[method.routing_key][1:,:],frqVals)) # concatenate new frequencies to already existing
-        return freqs,frqVals
+            slope, intercept, r, p, std_err = sps.linregress(x,np.asarray(k(-self.numPtsBack:-1))) # perform linear regression on last N hours (data tables continues to append until end of day)
+            if self.thrshldsTbls[ind]<0: # if hasn't crossed, continue to check against updated mean and std
+                Cross = self.predictCrossTime(np.mean(k)+2*np.std(k)) # predict time it will cross threshold if continues in this trend (threshold set as mean of last half hour plus 3*std of last half hour)
+            else: # if it crossed, threshold is not -1 and compare to last threshold (last mean+3std that it crossed)
+                Cross = self.predictCrossTime(self.thrshldsTbls[ind])
+            if Cross>0 and Cross<360 and slope>0: # If it will cross in less than 1 hour (number of 10 seconds in an hour)
+                self.counterTbls[:,ind] = np.concatenate((self.counterTbls[1:,ind],[1])) # append 1 to counter
+                self.thrshldsTbls[ind] =  np.mean(k)+2*np.std(k) # set threshold to this moment of anomaly
+                print("[%s]: %s, %s " % (self.name, self.timestmpsTbls[-1],frqVals[ind]))
+            else:
+                self.counterTbls[:,ind] = np.concatenate((self.counterTbls[method.routing_key][1:,ind],[0])) # append 0 to counter
+                self.thrshldsTbls[ind] =  -1 # reset so that threshold is updated next time it is anomal
+            if np.sum(self.counterTbls[-201:-1,ind]) >= 0.8*self.counterTbls[-201:-1].shape[0]: # if in the last N minutes it will cross in less than 1 hour in 80% of the times, alert
+                if ind<25: # it is a frequency
+                    txtstr = 'trend detected in freq %d [Hz]' %freqs[ind]
+                else: # it is the rms
+                    txtstr = 'trend detected in RMS'
+                self.plotDataAndRegression(k,txtstr,method.routing_key+self.timestmpsTbls[method.routing_key])
+                self.counterTbls[method.routing_key][:,ind] = np.zeros(200) # make zeros to not alert many times
+                print(txtstr)
+            ind+=1  
+        
+        
+    def DayEnd(self):  
+        ind = 0
+        self.bslnDist['N'] += 1
+        for k in self.dataTbls.T: # iterate the rows (25 freqs & RMS) of this channel  
+            Mu = np.mean(k)
+            Sig = np.std(k)
+            rng = np.arange(-10, 10, 0.001)
+            KLD = kl_divergence(norm.pdf(rng,self.bslnDist['mu'],self.bslnDist['signma']),norm.pdf(rng, Mu, Sig)) # calculate kullback leibnitz divergence of this day and the previous days
+            if KLD<100:
+                self.bslnDist['mu'][ind] = ((self.bslnDist['N']-1)*self.bslnDist['mu'][ind]+Mu)/self.bslnDist['N'] # calculate new mean
+                self.bslnDist['signma'][ind] = ((self.bslnDist['N']-1)*self.bslnDist['sigma'][ind]+Sig)/self.bslnDist['N'] # calculate new mean std 
+            else:
+                # send email that day is abnormal
+                pass
+            ind += 1
+        # --- make everything back to zero for new day
+        self.dataTbl = []
+        self.timestmps = []
+        self.counterTbl = np.zeros((self.numPtsBack,26))
+        self.thrshlds = -1*np.ones(26)
+        
+        
