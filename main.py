e5d09cd9d7520764a3def9102826060df66efed3
unknown <oricarmi92@gmail.com>
2020-02-13 11:32:44 +0200

    main file with channel class

--- main.py
+++ main.py
@@ -1,44 +1,197 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan  3 11:03:06 2020
+Created on Wed Jan  8 15:08:43 2020
 
 @author: Ori
 """
-
+import pika
+import json 
 import numpy as np
-import win32api
+import os.path
 from scipy import stats as sps
 from matplotlib import pyplot as plt
+from scipy.stats import norm
+import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+import datetime
 
-def linearModel(x):
-    return slope*x+intercept
-def predictCrossTime(threshold = 3):
+  
+def kl_divergence(p, q):
+    return np.sum(np.where(p != 0, p * np.log(p / q), 0))    
+def predictCrossTime(slope, intercept,threshold):
     return (threshold - intercept)/slope
-def plotData():
-    plt.plot(x,y)
-    plt.plot(x,list(map(linearModel,x)))
+
+class channell:
+     
+    def __init__(self, name,numPtsBack = 1080):
+        self.name = name[7:name.find('.V')]
+        self.numPtsBack = numPtsBack
+        self.dataTbl = [] # list of spectrums (max 8640 x 26)
+        self.counterTbl = np.zeros((200,26))   # binary counters if trend detected or not
+        self.timestmps = [] # timestamps of corresponding line in dataTbls/counterTbls
+        self.thrshlds = -1*np.ones(26) # thresholds of each frequency and RMS
+        self.x = np.arange(numPtsBack) # last numPtsBack points
+        self.bslnDist = {'N': 1, 'mu': np.zeros(26), 'sigma': np.zeros(26) }
     
-    
-freqs = [0.5,1,1.5,2,2.5,3.15,4,5,6.3,8,10,12.5,16,20,25,31.5,40,50,63,80,100,125,160,200,250]
-counter = np.zeros((30,26),dtype = int) # first 25 are freqs, 26 is energy
-x = np.arange(600)
-for k in range(counter.shape[1]): # iterate the frequencies
-    for i in range(4):
-        temp = np.random.normal(i*k/2,1,int(x.size/4))
-        if i == 0:
-            y = temp
+    def appendDataLine(self,data,timestmp):
+        if len(self.timestmps)==0:
+            self.dataTbl.append(data) # append to data tables (of day)
+            self.timestmps.append(timestmp) # append to timestamps array
+        elif datetime.datetime.date(self.timestmps[-1]) == datetime.datetime.date(timestmp): # if still on the same day
+            self.dataTbl.append(data) # append to data tables (of day)
+            self.timestmps.append(timestmp) # append to timestamps array
+            if len(self.timestmps)>8640:
+                self.dataTbl.pop(0)
+                self.timestmps.pop(0)
         else:
-            y = np.concatenate([y,temp])
-    slope, intercept, r, p, std_err = sps.linregress(x, y)
-    
-    Cross = predictCrossTime(4) 
-    if Cross>0 and Cross<360: # If it will cross in less than 1 hour (number of 10 seconds in an hour)
-        counter[29,k] +=1
-    else:
-        counter[29,k] = 0
-    if np.sum(counter[:,k]) >= 0.8*counter.shape[0]: # if in the last 5 minutes it will cross in less than 1 hour in 80% of the times
-        if k<len(freqs):
-            txtstr = 'trend detected in freq %d' %freqs[k]
-        else:
-            txtstr = 'trend detected in freq total energy'
-        win32api.MessageBox(0,txtstr,'alert')
+            self.DayEnd()
+        return
+            
+    def plotDataAndRegression(self,y,intercept,slope,r,TitlePlt,TitleWord,toSave = 1):
+        plt.figure() # plot the vector of numptsback and its regression line
+        plt.plot(self.x, y, 'o', label='original data')
+        plt.plot(self.x, intercept + slope*self.x, 'r', label='fitted line')
+        plt.legend()
+        plt.title(TitlePlt + ", " + TitleWord + " ,R-squared: %f" % r**2)
+        if toSave:
+            plt.savefig(os.path.join(pathImage))
+        plt.close()
+        sendEmail(TitlePlt + ' @ ' + TitleWord,pathImage) # call function to send email
+        append2log(TitleWord) # append anomal event to log
+        return
+
+   
+    def detectTrend(self):  
+        ind = 0 # index for frequencies and RMS ([0,25])
+        if len(self.dataTbl)<self.numPtsBack:
+            return
+        for k in np.asarray(self.dataTbl).T: # iterate the columns of the data and perform linear regression
+            kk = k[-self.numPtsBack:]
+            slope, intercept, r, p, std_err = sps.linregress(self.x,kk) # perform linear regression on last N hours (data tables continues to append until end of day)
+            if self.thrshlds[ind]<0: # if hasn't crossed, continue to check against updated mean and std
+                Cross = predictCrossTime(slope, intercept,np.mean(kk)+2*np.std(kk)) # predict time it will cross threshold if continues in this trend (threshold set as mean of last half hour plus 3*std of last half hour)
+            else: # if it crossed, threshold is not -1 and compare to last threshold (last mean+3std that it crossed)
+                Cross = predictCrossTime(slope, intercept,self.thrshlds[ind])
+            if Cross>0 and Cross<360 and slope>0: # If it will cross in less than 1 hour (number of 10 seconds in an hour)
+                self.counterTbl[:,ind] = np.concatenate((self.counterTbl[1:,ind],[1])) # append 1 to counter (of this column (freq or rms))
+                self.thrshlds[ind] =  np.mean(kk)+2*np.std(kk) # set threshold to this moment of anomaly
+                print("[%s]: %s, %s " % (self.name, self.timestmps[-1],freqs[ind]))
+            else:
+                self.counterTbl[:,ind] = np.concatenate((self.counterTbl[1:,ind],[0])) # append 0 to counter
+                self.thrshlds[ind] =  -1 # reset so that threshold is updated next time it is anomal
+            if np.sum(self.counterTbl[:,ind]) >= 0.8*self.counterTbl.shape[0]: # if in the last N minutes it will cross in less than 1 hour in 80% of the times, alert
+                if ind<25: # it is a frequency
+                    txtstr = 'trend detected in freq %d [Hz]' %freqs[ind]
+                else: # it is the rms
+                    txtstr = 'trend detected in RMS'
+                self.plotDataAndRegression(kk,intercept,slope,r,txtstr,self.name+' '+self.timestmps[-1].strftime("%m/%d/%Y, %H:%M:%S"))
+                self.counterTbls[:,ind] = np.zeros(200) # make zeros to not alert many times
+                print(txtstr)
+            ind+=1  
+        return
+        
+    def DayEnd(self):  
+        ind = 0
+        self.bslnDist['N'] += 1
+        for k in np.asarray(self.dataTbl).T: # iterate the rows (25 freqs & RMS) of this channel  
+            Mu = np.mean(k)
+            Sig = np.std(k)
+            rng = np.arange(-10, 10, 0.001)
+            if not self.bslnDist['mu'][ind]==0 and self.bslnDist['sigma'][ind]==0: # if not empty the distribution data
+                KLD = self.kl_divergence(norm.pdf(rng,self.bslnDist['mu'][ind],self.bslnDist['sigma'][ind]),norm.pdf(rng, Mu, Sig)) # calculate kullback leibnitz divergence of this day and the baseline distribution
+                if KLD<100: # if KL divergence is low (dists don't differ much), update baseline distribution
+                    self.bslnDist['mu'][ind] = ((self.bslnDist['N']-1)*self.bslnDist['mu'][ind]+Mu)/self.bslnDist['N'] # calculate new mean 
+                    self.bslnDist['sigma'][ind] = ((self.bslnDist['N']-1)*self.bslnDist['sigma'][ind]+Sig)/self.bslnDist['N'] # calculate new mean std 
+                else: # KL is high and this day's distribution is different than baseline (abnormal)
+                    if ind<25:
+                        txtstr = self.name + ' day ' + datetime.datetime.date(self.timestmps[-1]).strftime("%m/%d/%Y") + ', freq %d is abnormal' %freqs[ind]
+                    else:
+                        self.name + ' day ' + datetime.datetime.date(self.timestmps[-1]).strftime("%m/%d/%Y") + ', RMS is abnormal'
+                    sendEmail(txtstr,'')# send email that day is abnormal
+            else: # it is empty, initialize with this mu and sigma
+                self.bslnDist['mu'][ind] = Mu
+                self.bslnDist['sigma'][ind] = Sig
+            ind += 1
+
+
+def append2log(TitleWord):
+    with open(pathLog, 'a') as file:
+        file.write(TitleWord)
+    return
+def sendEmail(bodyTxt,imgPath):
+    gmail_user = 'ecomonitoring2@gmail.com'
+    gmail_password = 'monitoring1234'
+    to = ['Yair@eco-eng.co.il', 'Amit@eco-eng.co.il','ecomonitoring2@gmail.com'
+          ]
+    msg = MIMEMultipart()    # instance of MIMEMultipart 
+    msg['From'] = gmail_user # storing the senders email address       
+    msg['To'] = ", ".join(to) # storing the receivers email address  
+    msg['Subject'] = "Intel Anomaly Alert!" # storing the subject  
+    body = 'Trend Detected - ' + bodyTxt # string to store the body of the mail 
+    msg.attach(MIMEText(body, 'plain')) # attach the body with the msg instance 
+    filename = 'test.png' 
+    try:# try to open the image path and attach to MIME
+        attachment = open(imgPath, "rb") # open the file to be sent  
+        p = MIMEBase('application', 'octet-stream') # instance of MIMEBase and named as p 
+        p.set_payload((attachment).read())  # To change the payload into encoded form 
+        encoders.encode_base64(p)  # encode into base64 
+        p.add_header('Content-Disposition', "attachment; filename= %s" % filename) 
+        msg.attach(p) # attach the instance 'p' to instance 'msg' 
+    except: # if didn't succeed, continue to send email without attachment
+        pass    
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+#        server.starttls()
+        server.login(gmail_user, gmail_password)
+        text = msg.as_string() 
+        server.sendmail(gmail_user, to, text)
+        server.close()
+        print('email sent')
+    except:
+        print('Something went wrong...')
+    return
+
+def msg_receive(ch, method, properties, body):
+    ######## Parse incoming message #######
+    # Sensor name and channel is in the routing key (split on .)
+    # print("[%s]: %s " % (method.routing_key, data))
+    global channels,freqs,thisChannel
+    thisChannel = method.routing_key
+    if not thisChannel in channels:
+        channels[thisChannel] = channell(thisChannel)
+    data = json.loads(body) # parse the body of message received as json object
+    spectrum = data["results"][0]['values'] # get freq values
+    freqs = [] # frequencies list
+    frqVals = [] # frequencies values list
+    for f in spectrum.keys(): # iterate values and make one vector
+        try:
+            if float(f[0:-2])<=250:
+                freqs.append(float(f[0:-2]))
+                frqVals.append(float(spectrum[f])) 
+        except:
+            continue
+    frqVals.append(np.sqrt(np.mean(np.asarray(frqVals)**2))) # concatenate rms
+    channels[thisChannel].appendDataLine(frqVals,datetime.datetime.strptime(data["results"][0]['timestamp'][:-1], '%Y-%m-%dT%H:%M:%S.%f')) # append freq data and timestamp to this channel
+    channels[thisChannel].detectTrend()
+
+if __name__ == "__main__":
+    pathLog = r'C:\Users\Ori\Desktop\Ori\ECO\intel - python\anomaly detection\logfile.txt'#r'C:\Users\user\Desktop\Oric\ECO\intel - python\logfile.txt'
+    pathImage = r"C:\Users\Ori\Desktop\Ori\ECO\intel - python\anomaly detection\test.png"#r'C:\Users\user\Desktop\Oric\ECO\intel - python\test.png'#
+    if not 'channels' in locals():
+        channels = dict()
+    if not os.path.exists(pathLog):
+        open(pathLog, 'w').close()    
+    # Setup AMQP Connection
+    parameters = pika.URLParameters('amqps://eco-monitoring:p4_HVv%Mb6j&)P@amqp-ext.munisense.net:5671/eco-monitoring')
+    parameters.heartbeat = 0
+    print("Connecting AMQP..")
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    print("AMQP Connected!")
+    print('Starting consume...')
+    channel.basic_consume(queue="composite_results_pretty_v1", on_message_callback=msg_receive, auto_ack=True)
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
